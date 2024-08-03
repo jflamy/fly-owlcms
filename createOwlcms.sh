1ab08f188cf3a8d6b9e8817595d2e4819bd4dc52
Jean-François Lamy <jf@jflamy.dev>
2024-08-03 17:00:02 -0400

    skipped unneeded deploy that was interfering with attach

--- createOwlcms.sh
+++ createOwlcms.sh
@@ -3,20 +3,21 @@ export FLY_APP_root=$FLY_APP
 
 # create owlcms don't deploy because no database yet.
 rm -f fly.toml
-# has already been created in user interface to check for name conflict.
+# skip creation - has already been created in user interface to check for name conflict.
 #flyctl apps create --name $FLY_APP --org personal
 
-# create database and connect to owlcms. 
+# create database 
 export FLY_APP=${FLY_APP_root}-db
 flyctl postgres create --name $FLY_APP --initial-cluster-size 1 --vm-size shared-cpu-1x --volume-size 1 --region=$REGION --org personal
 
 # initial deploy owlcms
-export FLY_APP=$FLY_APP_root
-tmpfile=$(mktemp)
-envsubst < owlcms.toml > $tmpfile
-flyctl deploy --ha=false --config $tmpfile
-rm -f $tmpfile
+#export FLY_APP=$FLY_APP_root
+#tmpfile=$(mktemp)
+#envsubst < owlcms.toml > $tmpfile
+#flyctl deploy --ha=false --config $tmpfile
+#rm -f $tmpfile
 
+# connect database to owlcms
 set +x
 echo
 echo "***** The next step can take a couple minutes.  Please be patient *****"
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -
-fly deploy . --app owlcms-cloud --config owlcms-cloud.toml --ha=false --image-label $1
+fly deploy . --local-only --app owlcms-cloud --config owlcms-cloud.toml --ha=false --image-label $1
